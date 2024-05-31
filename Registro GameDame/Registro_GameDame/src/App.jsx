7870e00491753fd8530e1b089fa08755e815e808
Felipe Crisostomo <felipecrisostomo_@hotmail.com>
2024-05-31 17:39:07 -0300

    Modelo Inicial

--- Registro GameDame/Registro_GameDame/src/App.jsx
+++ Registro GameDame/Registro_GameDame/src/App.jsx
@@ -1,15 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [registro, setRegistro] = useState([
+    {
+      id: 1,
+      text: "Nome de Usuário",
+    },
+    {
+      id: 2,
+      text: "E-mail",
+    },
+    {
+      id: 3,
+      text: "CPF",
+    },
+    {
+      id: 4,
+      text: "Senha",
+    },
+    {
+      id: 5,
+      text: "Confirmar Senha",
+    },
+  ])
 
   return (
-    <>
-      
-    </>
+    <div className="app">
+      <h1>Registrar</h1>
+      <div className="bloco-registro">
+        {registro.map((registro) => (
+          <div className="registro">
+            <div className="content">
+              <p>{registro.text}</p>
+            </div>
+          </div>
+        ))}
+        <div className="botao-seta">
+          <button>→</button>
+        </div>
+        <div className="botao-login">
+          <button>Fazer Login</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
