e6d16905b11c03eeac3553a407ebf3bed8bbde0f
Felipe Crisostomo <felipecrisostomo_@hotmail.com>
2024-05-31 21:40:13 -0300

    Registro (Bruto) Finalizado

--- Registro GameDame/Registro_GameDame/src/App.jsx
+++ Registro GameDame/Registro_GameDame/src/App.jsx
@@ -3,47 +3,38 @@ import './App.css';
 
 
 function App() {
-  const [registro, setRegistro] = useState([
-    {
-      id: 1,
-      text: "Nome de Usuário",
-    },
-    {
-      id: 2,
-      text: "E-mail",
-    },
-    {
-      id: 3,
-      text: "CPF",
-    },
-    {
-      id: 4,
-      text: "Senha",
-    },
-    {
-      id: 5,
-      text: "Confirmar Senha",
-    },
-  ])
 
   return (
-    <div className="app">
-      <h1>Registrar</h1>
-      <div className="bloco-registro">
-        {registro.map((registro) => (
-          <div className="registro">
-            <div className="content">
-              <p>{registro.text}</p>
-            </div>
-          </div>
-        ))}
-        <div className="botao-seta">
-          <button>→</button>
+    <div className='container'>
+      <form className='Registro'>
+        <h1>Registrar</h1>
+        <div className="register-box">
+          <input type="text" name='' required />
+          <label>Nome do Usuário</label>
         </div>
-        <div className="botao-login">
-          <button>Fazer Login</button>
+        <div className="register-box">
+          <input type="email" name='' required />
+          <label>E-mail</label>
         </div>
-      </div>
+        <div className="register-box">
+          <input type="number" name='' required />
+          <label>CPF</label>
+        </div>
+        <div className="register-box">
+          <input type="password" name='' required />
+          <label>Senha</label>
+        </div>
+        <div className="register-box">
+          <input type="password" name='' required />
+          <label>Confirmar Senha</label>
+        </div>
+        <div>
+          <a href="#" className='seta'>→</a>
+        </div>
+        <div>
+          <a href="#" className="login">Fazer Login</a>
+        </div>
+      </form>
     </div>
   )
 }
